d68b2caf962b76caee042c213f7a1bf8640d8db0
Rajat Singh <rajatsingh.r@udaan.com>
2022-11-19 22:09:01 +0530

    Time: 48 ms (84.18%), Space: 19.6 MB (35.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    
-    static bool compareInterval(vector<int>a,vector<int>b){
-        return a[0]<b[0];
-    }
-    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),compareInterval);
-        vector<vector<int>>result;
-        result.push_back(intervals[0]);
+        
+        vector<vector<int>>res;
+        
+        sort(intervals.begin(),intervals.end());
+        
+        res.push_back(intervals[0]);
+        
         for(int i=1;i<intervals.size();i++)
         {
-            if(result[result.size()-1][1] < intervals[i][0])
-             result.push_back(intervals[i]);
-            else {
-                int low = min(result[result.size()-1][0],intervals[i][0]);
-                int high = max(result[result.size()-1][1],intervals[i][1]);
-                result.pop_back();
-                result.push_back(vector<int>{low,high});
+            if(res[res.size()-1][1]<intervals[i][0])
+                res.push_back(intervals[i]);
+            else
+            {   
+                int m = min(res[res.size()-1][0],intervals[i][0]);
+                    int n = max(res[res.size()-1][1],intervals[i][1]);
+                res.pop_back();
+                res.push_back({m,n});    
             }
         }
-        return result;
+        return res;
     }
 };

467a9aead1b71e1bf8b440f8dd22912ff85cd32e
Rajat Singh <rajatsingh.r@udaan.com>
2022-11-24 21:34:56 +0530

    Time: 372 ms (23.96%), Space: 61.2 MB (92.09%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,39 +2,19 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
-//         int tortoise = nums[0];
-//         int hare = nums[0];
-        
-//         while(tortoise!=hare)
-//         {
-//             hare=nums[nums[hare]];
-//             tortoise=nums[tortoise];
-//             if(hare==tortoise)
-//                 break;
-//         }
-//         hare=nums[0];
-//         while(tortoise!=hare)
-//         {
-//             hare=nums[nums[hare]];
-//             tortoise=nums[tortoise];
-//         }
-//         return hare;
-        
         int tortoise = nums[0];
         int hare = nums[0];
-
-        do {
-            tortoise = nums[tortoise];
-            hare = nums[nums[hare]];
-        } while (tortoise != hare);
-
-        // Find the "entrance" to the cycle.
-        tortoise = nums[0];
-        while (tortoise != hare) {
-            tortoise = nums[tortoise];
-            hare = nums[hare];
+        
+        do{
+            hare=nums[nums[hare]];
+            tortoise=nums[tortoise];
+        }while(hare!=tortoise);
+        hare=nums[0];
+        while(tortoise!=hare)
+        {
+            hare=nums[hare];
+            tortoise=nums[tortoise];
         }
-
         return hare;
     }
 };

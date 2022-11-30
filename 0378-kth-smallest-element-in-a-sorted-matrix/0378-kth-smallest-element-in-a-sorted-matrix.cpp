9d3af3e321d7773a03850647ae1a8dc82242fa2a
Rajat Singh <rajatsingh.r@udaan.com>
2022-11-30 19:45:57 +0530

    Time: 47 ms (79.91%), Space: 14.4 MB (37.80%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     
-    int findLessOrEqual(int val,vector<vector<int>>& matrix)
-    {
-        int count =0;
-        for(int i=0;i<matrix.size();i++)
-            count += upper_bound(matrix[i].begin(),matrix[i].end(),val)-matrix[i].begin();
-        return count;
-    }
+    // int findLessOrEqual(int val,vector<vector<int>>& matrix)
+    // {
+    //     int count =0;
+    //     for(int i=0;i<matrix.size();i++)
+    //         count += upper_bound(matrix[i].begin(),matrix[i].end(),val)-matrix[i].begin();
+    //     return count;
+    // }
     
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int l = matrix[0][0],m=matrix.size(),r=matrix[m-1][m-1];
-        while(l<=r)
-        {
-            int mid = l+(r-l)/2;
-            int count = findLessOrEqual(mid,matrix);
-            if(count<k)
-                l=mid+1;
-            else 
-                r=mid-1;
-        }
-        return l;
+        // int l = matrix[0][0],m=matrix.size(),r=matrix[m-1][m-1];
+        // while(l<=r)
+        // {
+        //     int mid = l+(r-l)/2;
+        //     int count = findLessOrEqual(mid,matrix);
+        //     if(count<k)
+        //         l=mid+1;
+        //     else 
+        //         r=mid-1;
+        // }
+        // return l;
+        int n = matrix.size();
+        priority_queue<int> maxH;
+  if(k==1)
+    return matrix[0][0];
+   
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n;j++)
+    {
+      maxH.push(matrix[i][j]);
+      if(maxH.size()>k)
+        maxH.pop();
+    }
+  }
+   
+  return maxH.top();
     }
 };
